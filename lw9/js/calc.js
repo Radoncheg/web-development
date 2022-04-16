4749e6ac626fd0bbcea460aacda32a4b3b2e9d9c
Radoncheg <oshaev@gmail.com>
2022-04-17 00:15:27 +0300

    lw 9.2 добавил ошибку деления на ноль, и поддержку float

--- lw9/js/calc.js
+++ lw9/js/calc.js
@@ -1,5 +1,3 @@
-let isCorrect;
-
 function checkClosedBrackets(str) {
   let openedBracketsCounter = 0;
   for (let i = 0; i < str.length; i++) {
@@ -23,46 +21,46 @@ function makeCalc(action, a, b) {
     case '*':
       return a * b;
     case '/':
-      return a / b;
-    default: {
-      console.log('Ошибка ввода. Введите действие!');
-      isCorrect = false;
+      if (b !== 0) return a / b;
+      else console.log('Ошибка! деление на ноль!');
     }
-  }
 }
+
 
 function calc(str) {
   const actions = ['+', '-', '*', '/'];
   let result;
   let isOver;
-  isCorrect = true;
   let stack = [];
   let b = "";
 
   if (!checkClosedBrackets(str)) {
-      console.log("Ошибка ввода. Проверьте скобки!");
+      console.log('Ошибка ввода. Проверьте скобки!');
       return;
   }
 
   for (let i = 0; i < str.length; i++) {
     if (actions.indexOf(str[i]) !== -1) stack.push(str[i]);
-    else
-      if (!isNaN(parseInt(str[i]))) {
-        b += str[i];
-        if (isNaN(parseInt(str[i+1]))) {
-          while (!isNaN(parseInt(stack[stack.length-1]))) {
-            const a = stack.pop();
-            const action = stack.pop();
-            b = makeCalc(action, parseInt(a), parseInt(b));
+    else if (!isNaN(parseInt(str[i]))) {
+      b += str[i];
+      if (isNaN(parseInt(str[i+1]))) {
+        while (!isNaN(parseInt(stack[stack.length-1]))) {
+          const a = stack.pop();
+          const action = stack.pop();
+          if (action === undefined) {
+            console.log('Ошибка ввода. Пропущено действие');
+            return;
           }
-          stack.push(b);
-          b = "";
+          else b = makeCalc(action, parseFloat(a), parseFloat(b));
         }
+        stack.push(b);
+        b = "";
       }
     }
+  }
 
   result = stack.pop();
   isOver = stack.pop();
-  if ((isCorrect) && (isOver === undefined)) console.log(result);
-  else if (isCorrect) console.log("Ошибка ввода. Аргумент должен быть целочисленным!");
+  if (isOver === undefined) console.log(result);
+  else console.log('Ошибка ввода. Некорректные аргументы!');
 }
