ba53d64599a4a4605372892cd5181ff28af68372
Radoncheg <oshaev@gmail.com>
2022-04-27 15:21:48 +0300

    lw 10 done

--- lw10/js/script.js
+++ lw10/js/script.js
@@ -6,67 +6,53 @@ function main() {
     const topbtn = document.querySelector('.top__button');
     const close = document.querySelector('.register__close-button');
     const overlayDiv = document.createElement('div');
-
+    const selectActivityMenuOptions = document.querySelectorAll('.select-activity__menu_option');
     const selectActivityContainer = document.querySelector('.select-container');
     const selectActivityInput = document.getElementById('select-activity');
     const selectActivityMenu = document.querySelector('.select-activity__menu');
-
     overlayDiv.classList.add('overlay');
     overlayDiv.addEventListener('click', onPopupClose);
 
     let menuBtn = document.querySelector('.menu-btn');
     let menu = document.querySelector('.navigation');
 
-    menuBtn.addEventListener('click', function()
-    {
+    menuBtn.addEventListener('click', function () {
         menu.classList.toggle('active');
         menuBtn.classList.toggle('active')
     })
 
 
-    selectActivityInput.addEventListener('click', function(ev)
-    {
+    selectActivityInput.addEventListener('click', function (ev) {
         selectActivityContainer.classList.toggle('expanded');
         updateOptions()
     })
 
-    selectActivityMenu.addEventListener('click', function(ev)
-    {
+    selectActivityMenu.addEventListener('click', function (ev) {
         const value = ev.target.dataset.activity;
-        if (value)
-        {
+        if (value) {
             selectActivityInput.value = value
         }
         selectActivityContainer.classList.remove('expanded')
     })
 
-    const selectActivityMenuOptions = document.querySelectorAll('.select-activity__menu_option');
-
-    function updateOptions()
-    {
+    function updateOptions() {
         const currentValue = selectActivityInput.value
-        selectActivityMenuOptions.forEach(option =>
-        {
-            if (currentValue === option.dataset.activity)
-            {
+        selectActivityMenuOptions.forEach(option => {
+            if (currentValue === option.dataset.activity) {
                 option.classList.add('option__checked')
-            }
-            else
-            {
+            } else {
                 option.classList.remove('option__checked')
             }
         })
     }
 
-    function onClick()
-    {
+    function onClick() {
         document.body.appendChild(overlayDiv);
         popup.classList.add('show');
         document.body.style.overflow = "hidden";
     }
 
-    function onPopupClose()
-    {
+    function onPopupClose() {
         document.body.removeChild(overlayDiv);
         popup.classList.remove('show');
         document.body.style.overflow = "";
